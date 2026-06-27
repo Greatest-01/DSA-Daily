58c0931ba77c546bffe248d4ff6506ce39513299
Greatest-01 <aryanydv649@gmail.com>
2026-06-28 03:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (67.13%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+        void findcomb(int ind,vector<int>& arr,vector<int>&ds,        vector<vector<int>> &ans){
+            
+                ans.push_back(ds);   
+                for(int i=ind;i<arr.size();i++){
+                    if( i>ind && arr[i]==arr[i-1] ) continue;
+                    ds.push_back(arr[i]);
+                    findcomb(i+1,arr,ds,ans);
+                    ds.pop_back();
+
+                }
+            }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int>ds;
+        findcomb(0,nums,ds,ans);
+        return ans ;
+    }
+};
